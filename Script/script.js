7f470488af44d5ccaeede04b7dd9b9cd1c99566b
lysim-max <152422298+lysim-max@users.noreply.github.com>
2024-02-24 12:18:41 -0500

    Update 7
    Added function to let user login and go to degree selection page and made it to where when choosing a degree the page will open to that degree form.

--- Script/script.js
+++ Script/script.js
@@ -1,1 +1,24 @@
-/*Javascript page for incorporating the AI and Connecting the website to database*/ 
+/*Javascript page for incorporating the AI and Connecting the website to database*/
+function loginFunc(){
+    var user= document.getElementById("username").value;
+    var pass= document.getElementById("password").value;
+    if(user=="AdminDemo" && pass=="Demopass123!"){
+        window.location.assign("degree_selection.html");
+        alert("Login Successful");
+
+    }
+    else{
+        alert("Invalid Login");
+        return;
+    }
+
+} 
+var degreeMenu = document.getElementById('degree');
+    degreeMenu.onchange = function(){
+var userOption = this.options[this.selectedIndex];
+    if (userOption.value != "nothing")
+    {
+    window.open(userOption.value, "Choose a degree Computer Science Information Technology Business", "");
+    }
+
+}
